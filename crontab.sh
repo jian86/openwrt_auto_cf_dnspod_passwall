159f8fa8fee513e5bbfe59f61d8f0c1aeb0aa5c2
jian86 <jian86@gmail.com>
2023-12-18 23:32:47 +0800

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-
-# 设置测试用的代理服务器地址和端口
-proxy_server="127.0.0.1:1081"
-test_website="www.google.com"
+source /root/youxuan/config.sh
+# # 设置测试用的代理服务器地址和端口
+# proxy_server="127.0.0.1:1081"
+# url_website="www.google.com"
 
 
 
 # 设置测试次数上限
-max_retries=10
-retry_count=0
+url_max_retries=10
+url_retry_count=0
 
 # 循环测试代理服务器是否能访问指定网站
-while [ $retry_count -lt $max_retries ]; do
-    if curl -x socks5h://$proxy_server -s --head -m 1 $test_website | head -n 1 | grep "200 OK" > /dev/null; then
+while [ $url_retry_count -lt $url_max_retries ]; do
+    if curl -x socks5h://$proxy_server -s --head -m 1 $url_website | head -n 1 | grep "200 OK" > /dev/null; then
 	current_time1=$(date "+%Y-%m-%d %H:%M:%S")
 	echo "#####################################################################################################"
-        echo "$current_time1 代理服务器 $proxy_server 可以访问 $test_website."
+        echo "$current_time1 代理服务器 $proxy_server 可以访问 $url_website."
         exit 0
     else
 	current_time2=$(date "+%Y-%m-%d %H:%M:%S")
 	echo "#####################################################################################################"
-        echo "$current_time2 代理服务器 $proxy_server 不能访问 $test_website. 尝试次数: $((retry_count + 1))"
-        retry_count=$((retry_count + 1))
+        echo "$current_time2 代理服务器 $proxy_server 不能访问 $url_website. 尝试次数: $((url_retry_count + 1))"
+        url_retry_count=$((url_retry_count + 1))
         sleep 1  # 暂停1秒后再次测试
     fi
 done
@@ -29,5 +29,5 @@ done
 # 达到最大尝试次数后执行执行Edtunnel脚本脚本
 current_time3=$(date "+%Y-%m-%d %H:%M:%S")
 echo "#####################################################################################################"
-echo "$current_time3 连续 $max_retries 次无法访问 $test_website，执行Edtunnel脚本..."
-sh /root/youxuan/edtunnel.sh
+echo "$current_time3 连续 $url_max_retries 次无法访问 $url_website，执行Edtunnel脚本..."
+sh $DATA_DIR/edtunnel.sh
